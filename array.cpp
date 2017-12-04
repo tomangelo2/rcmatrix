524cadae58087392eeedd88c78498ee1d2e35422
tomangelo2 <tomangelo@wp.pl>
2017-12-05 00:32:42 +0100

    CArray class

---
+++ array.cpp
@@ -0,0 +1,102 @@
+#include "array.h"
+
+CArray::CArray(int h, int w, double v, double f): m_col(w), m_row(h)
+{
+    int i = 0;
+    A = new double*[h];
+    try
+    {
+        for(i = 0; i < h; i++)
+        {
+            A[i] = new double[w];
+        }
+    }catch(...)
+    {
+        while(i > 0)
+        {
+            delete A[--i];
+        }
+        delete A;
+        throw;
+    }
+    for (i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w; j++)
+        {
+            if(i == j)
+            {
+                A[i][i] = v;
+            }
+            else
+            {
+                A[i][j] = f;
+            }
+        }
+    }
+}
+
+CArray::CArray(std::fstream &f)
+{
+    int i;
+    if(!f.is_open())
+    {
+        throw file_not_open();
+    }
+
+
+    f >> this->m_row >> this->m_col;
+
+    try
+    {
+        A = new double*[m_row];
+    }catch(...)
+    {
+        throw bad_alloc();
+    }
+    try
+    {
+        for(i = 0; i < m_row; i++)
+        {
+            A[i] = new double[m_col];
+        }
+    }catch(...)
+    {
+        while(i > 0)
+        {
+            delete A[--i];
+        }
+        delete A;
+        throw bad_alloc();
+    }
+
+
+    if(this->m_row <= 0 || this->m_row <= 0)
+    {
+        delete A;
+        throw WrongDim();
+    }
+
+    for(int i = 0; i < this->m_row; i++)
+    {
+        for(int j = 0; i < this->m_col; i++)
+        {
+            if(f.good())
+                f >> A[i][j];
+            else
+            {
+                delete A;
+                throw file_error();
+            }
+        }
+    }
+}
+
+CArray::CArray()
+{
+
+}
+
+CArray::~CArray()
+{
+
+}
---
+++ array.h
@@ -0,0 +1,27 @@
+#ifndef CARRAY_H
+#define CARRAY_H
+
+#include <fstream>
+
+class CArray
+{
+public:
+    CArray(int h, int w, double v, double f);
+    CArray(std::fstream &f);
+    CArray();
+    ~CArray();
+
+    double** A;
+    int m_col, m_row; //wymiary
+    unsigned int n; //ilość wystąpień
+};
+
+/*Wyjątki*/
+class IndexOutOfRange{};
+class WrongDim{};
+class bad_alloc{};
+class null_as_argument{};
+class file_error{};
+class file_not_open{};
+
+#endif // CARRAY_H
